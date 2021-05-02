e58be3983684c4f0bdd541e6e83cc96d6bde0aa0
rushin92 <71931021+rushin92@users.noreply.github.com>
2021-05-02 15:16:56 +0530

    app lab

---
+++ Home/code.js
@@ -0,0 +1,34 @@
+onEvent("Back", "click", function( ) {
+  setScreen("screen1");
+});
+onEvent("Next", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("Search", "click", function( ) {
+  open("https://rushin92.github.io/static");
+});
+onEvent("Xmusic", "click", function( ) {
+  open("https://rushin92.github.io");
+});
+onEvent("travel", "click", function( ) {
+  open("https://rushin92.github.io/travel");
+});
+onEvent("Dinosaurs", "click", function( ) {
+  open("https://rushin92.github.io/dinosaurs");
+});
+onEvent("Dragons", "click", function( ) {
+  open("https://rushin92.github.io/dragons");
+});
+onEvent("Gosts", "click", function( ) {
+  open("https://rushin92.github.io/ghosts");
+});
+onEvent("Vamp", "click", function( ) {
+  open("https://rushin92.github.io/vampires");
+});
+setText("text_input1", "Your name here del and then type");
+onEvent("Set", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("id", "click", function( ) {
+  setScreen("screen1");
+});
